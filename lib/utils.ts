70e95fb9f44fa16f4196143678e8f238e9b0864c
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2024-01-05 18:30:52 +0100

    add vercel redirect url

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export function absoluteUrl(path: string) {
 	if (typeof window !== 'undefined') return path;
 	if (process.env.VERCEL_URL)
-		return `https://${process.env.VERCEL_URL}${path}`;
+		return `https://chat-with-pdf-six.vercel.app${path}`;
 	return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
 export function constructMetadata({
